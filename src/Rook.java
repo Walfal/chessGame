3f7cb38bba6dda1ba4da515b5390d482ded82840
Walfal <raphael.durandy@isep.fr>
2021-12-06 14:19:33 +0100

    Better isValidMove Rook

--- src/Rook.java
+++ src/Rook.java
@@ -7,37 +7,35 @@ public class Rook extends Piece {
 	@Override
 	public boolean isValidMove(Position position, Cell[][] board) {
 		if (position.getRow() > super.position.getRow() && position.getColumn() == super.position.getColumn()) {
-			for(int i = super.position.getRow() + 1; i< position.getRow(); i++) {
-				if(board[i][position.getColumn()].getCurrentPiece() != null) {
-					return false;
-				}
-			}
+      for (int i = super.position.getRow() + 1; i < position.getRow(); i++) {
+        if (board[i][position.getColumn()].getCurrentPiece() != null) {
+          return false;
+        }
+      }
+      return true;
 		} else if (position.getColumn() > super.position.getColumn() && position.getRow() == super.position.getRow()) {
-			for(int i = super.position.getColumn() + 1; i< position.getColumn(); i++) {
+			for(int i = super.position.getColumn() + 1; i < position.getColumn(); i++) {
 				if(board[position.getRow()][i].getCurrentPiece() != null) {
 					return false;
 				}
 			}
+      return true;
 		} else if (position.getRow() < super.position.getRow() && position.getColumn() == super.position.getColumn()) {
 			for(int i = position.getRow() + 1; i < super.position.getRow(); i++) {
 				if(board[i][position.getColumn()].getCurrentPiece() != null) {
 					return false;
 				}
 			}
+      return true;
 		} else if (position.getColumn() < super.position.getColumn() && position.getRow() == super.position.getRow()) {
 			for(int i = position.getColumn() + 1; i < super.position.getColumn(); i++) {
 				if(board[position.getRow()][i].getCurrentPiece() != null) {
 					return false;
 				}
 			}
-			
+      return true;
 		}	
-		if (board[position.getRow()][position.getColumn()].getCurrentPiece() != null) {
-			if(board[position.getRow()][position.getColumn()].getCurrentPiece().color == super.color) return false;
-		}
-		
-		
-		return true;
+    return false;
 	}
 
 	@Override
