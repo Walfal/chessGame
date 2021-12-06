78a156efc4727c2d92eb02436cb1862ba1ff8f57
Walfal <raphael.durandy@isep.fr>
2021-12-06 14:41:00 +0100

    isValidMove Bishop

--- src/Bishop.java
+++ src/Bishop.java
@@ -2,13 +2,46 @@ public class Bishop extends Piece {
 
 	public Bishop(int color, Position position) {
 		super(color, position);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public boolean isValidMove(Position position, Cell[][] board) {
-		// TODO Auto-generated method stub
-		return false;
+  public boolean isValidMove(Position position, Cell[][] board) {
+    int[] dest = { position.getRow(), position.getColumn() };
+    if (Math.abs(dest[0] - super.position.getRow()) != Math.abs(dest[1] - super.position.getColumn())) {
+      return false;
+    }
+
+    int deep = Math.abs(position.getRow() - super.position.getRow()) - 1;
+		if (dest[0] > super.position.getRow() && dest[1] > super.position.getColumn()) {
+      for (int i = 1; i < deep; i++) {
+        if (board[dest[0] - i][dest[1] - i].getCurrentPiece() != null) {
+          return false;
+        }
+      }
+      return true;
+		} else if (dest[0] > super.position.getRow() && dest[1] < super.position.getColumn()) {
+      for (int i = 1; i < deep; i++) {
+        if (board[dest[0] - i][dest[1] + i].getCurrentPiece() != null) {
+          return false;
+        }
+      }
+      return true;
+		} else if (dest[0] < super.position.getRow() && dest[1] > super.position.getColumn()) {
+      for (int i = 1; i < deep; i++) {
+        if (board[dest[0] + i][dest[1] - i].getCurrentPiece() != null) {
+          return false;
+        }
+      }
+      return true;
+		} else if (dest[0] < super.position.getRow() && dest[1] < super.position.getColumn()) {
+      for (int i = 1; i < deep; i++) {
+        if (board[dest[0] + i][dest[1] + i].getCurrentPiece() != null) {
+          return false;
+        }
+      }
+      return true;
+		}	
+    return false;
 	}
 
 	@Override
